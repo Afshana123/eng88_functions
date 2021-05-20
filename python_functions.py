591d345e84518e1e75a40233cef3030e6b71210e
Afshana123 <nKafuk2JTFW!ATJ>
2021-05-20 16:44:39 +0100

    Updated

--- python_functions.py
+++ python_functions.py
@@ -1,53 +1,54 @@
-# Let's create a basic function to gree
-# Syntax def name()
+### Let's create a basic function to greet user
+### Syntax def name()
 
-# First Iteration
-# def greetings():
-#     return "Welcome to Cyber Security!" # you need to call the funtion in order for you to execute the command
-#
-# print(greetings())
+### First Iteration
 
-# Second Iteration
+def greetings():
+    return "Welcome to Cyber Security!" # you need to call the funtion in order for you to execute the command
 
-# def greeting_user(name):
-#      return "Welcome on board!"
-#
-# print(greeting_user("engineering 88"))
+print(greetings())
 
-# Exercise: Take user name as input() and display it back to them with greeting messages of your choice
+### Second Iteration
 
-# user_name = str(input("What is your name? "))
-#
-# def greet_user(user_name):
-#     return "Welcome " + user_name
-#
-# print(greet_user(you can put anything in here))
+def greeting_user(name):
+     return "Welcome on board!"
 
-# Creating a function that takes 2 args as ints
+print(greeting_user("engineering 88"))
+
+### Exercise: Take user name as input() and display it back to them with greeting messages of your choice
+
+user_name = str(input("What is your name? "))
+
+def greet_user(user_name):
+    return "Welcome " + user_name
+
+print(greet_user(you can put anything in here))
+
+### Creating a function that takes 2 args as ints
 
 def add(value1, value2):
     return value1 + value2
 
 print(add(2,3))
 
-def add(value1, value2):
+def subtract(value1, value2):
     return value1 - value2
 
 print(add(9,5))
 
-def add(value1, value2):
+def multiply(value1, value2):
     return value1 * value2
 
 print(add(2,3))
 
-def add(value1, value2):
+def divide(value1, value2):
     return value1 / value2
 
 print(add(8,2))
 
-# Create a function that calculates the percentage
+#### Create a function that calculates the percentage
 
-def add(value1, value2):
-    return Value1/100
+def percentage(value1, value2):
+    return (value1/value2) * 100
 
-print(add(2,3))
+print(percentage(2,3))
